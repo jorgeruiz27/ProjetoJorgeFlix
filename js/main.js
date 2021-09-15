c9627182a40a257019b012283536d18c24a20d86
jorgeruiz27 <jorge.cx.27@gamil.com>
2021-09-15 01:22:49 -0400

    versao 1.1

---
+++ js/main.js
@@ -0,0 +1,51 @@
+function clickMenu(){
+    alert("Pagina em Desenvolvimento...");
+}
+
+function clickAssistir(){
+    window.open("https://www.youtube.com/watch?v=R1v0uFms68U/");
+}
+
+function clickInfo(){
+    window.open("https://pt.wikipedia.org/wiki/The_Walking_Dead_(s%C3%A9rie_de_televis%C3%A3o)");
+}
+
+function clickMini1(){
+    window.open("https://www.youtube.com/watch?v=_uUcMwsR5hg");
+}
+
+function clickMini2(){
+    window.open("https://www.youtube.com/watch?v=xdBWrkcmMwU");
+}
+
+function clickMini3(){
+    window.open("https://www.youtube.com/watch?v=R1v0uFms68U/");
+}
+
+function clickMini4(){
+    window.open("https://www.youtube.com/watch?v=2wqzcLyaU8c");
+}
+
+function clickMini5(){
+    window.open("https://www.youtube.com/watch?v=X4bF_quwNtw");
+}
+
+function clickMini6(){
+    window.open("https://www.youtube.com/watch?v=stXs0Sf0f8A");
+}
+
+function clickMini7(){
+    window.open("https://www.youtube.com/watch?v=tUeo0RZN9kM");
+}
+
+function clickMini8(){
+    window.open("https://www.youtube.com/watch?v=xxmDXE1yxjs");
+}
+
+function clickMini9(){
+    window.open("https://www.youtube.com/watch?v=uNAxHLp7wv8");
+}
+
+function clickMini10(){
+    window.open("https://www.youtube.com/watch?v=HRV6tMR-SSs");
+}
